ba9751d8c41d9e01aad09ecbed20854c655f9c36
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-03-14 17:21:42 +0100

    1

---
+++ LessTeoryProject/src/main/java/ProgettoLessTheory/LessTeoryProjectApplication.java
@@ -0,0 +1,15 @@
+package ProgettoLessTheory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessTeoryProjectApplication {
+
+	public static void main(String[] args) {
+		//Fa eseguire tutto il progetto spring (controller, entity, repositery e simili)
+
+		SpringApplication.run(LessTeoryProjectApplication.class, args);
+	}
+
+}
---
+++ LessTeoryProject/src/main/java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/") //dice che questo metodo seguente risponde ad un determinato path
+	@ResponseBody  //tutto il contenuto di questo metodo verrà restituito all'utente
+	public String index() {
+					return "800A";
+			}
+}

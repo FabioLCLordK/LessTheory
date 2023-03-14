b54dcacee7677d4fe74369b62bdfd61496afdbcc
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-03-14 15:58:05 +0100

    vers 1.02

---
+++ LessTeory/src/main/java/ProgettoLessTheory/LessTeoryApplication.java
@@ -0,0 +1,15 @@
+package ProgettoLessTheory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessTeoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessTeoryApplication.class, args);
+		
+		//PROVA SA SA
+	}
+
+}
